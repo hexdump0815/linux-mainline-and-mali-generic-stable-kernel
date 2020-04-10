d91e056b985f0df048ee831053a880c49d8808dd
hexdump <hexdump0815@googlemail.com>
2020-04-10 19:35:44 +0200

    new nyan big try - not yet working

---
+++ misc.cbt/options/enable-additional-options.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /compile/source/linux-stable
+
+./scripts/config -d CONFIG_EXT2_FS
+./scripts/config -d CONFIG_EXT3_FS
+./scripts/config -d CONFIG_BLK_DEV_RAM
+./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
+./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
+
+for i in `cat /compile/doc/stable/misc.cbt/options/additional-options-yes.txt`; do
+  echo $i
+  ./scripts/config -e $i
+done
+
+for i in `cat /compile/doc/stable/misc.cbt/options/additional-options-mod.txt`; do
+  echo $i
+  ./scripts/config -m $i
+done
