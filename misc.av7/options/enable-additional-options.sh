c3eb3be2d02f06b186045bd891825c89069ce143
hexdump <hexdump0815@googlemail.com>
2020-08-23 15:11:30 +0200

    v5.4.58 - fresh multi_v7_defconfig + opts incl. pl2303

--- misc.av7/options/enable-additional-options.sh
+++ misc.av7/options/enable-additional-options.sh
@@ -8,6 +8,10 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
+# disable lima and panfrost for now
+./scripts/config -d CONFIG_DRM_LIMA
+./scripts/config -d CONFIG_DRM_PANFROST
+./scripts/config -d CONFIG_DRM_GEM_SHMEM_HELPER
 
 for i in `cat /compile/doc/stable/misc.av7/options/additional-options-yes.txt`; do
   echo $i
