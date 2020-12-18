7933cf81ef8b0f91bec90cde73ddf508dd241c4f
hexdump <hexdump0815@googlemail.com>
2020-12-18 20:23:58 +0100

    v5.10.1 exy, exynos_defconfig based, lima, mali optional

--- misc.exy/options/enable-additional-options.sh
+++ misc.exy/options/enable-additional-options.sh
@@ -10,8 +10,6 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
-./scripts/config -d CONFIG_DRM_TEGRA
-./scripts/config -d CONFIG_DRM_NOUVEAU
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0x0
 ./scripts/config -d CONFIG_DRM_TEGRA
 ./scripts/config -d CONFIG_DRM_NOUVEAU
@@ -21,6 +19,12 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_XFS_FS
 ./scripts/config -d CONFIG_REISERFS_FS
 ./scripts/config -d CONFIG_OCFS2_FS
+
+./scripts/config -d CONFIG_MALI400_PROFILING
+./scripts/config -d CONFIG_MALI_DMA_BUF_MAP_ON_ATTACH
+
+./scripts/config -d CONFIG_LOCALVERSION_AUTO
+./scripts/config --set-str CONFIG_LOCALVERSION "-stb-exy"
 
 for i in `cat /compile/doc/stable/misc.exy/options/additional-options-*-yes.txt`; do
   echo $i
