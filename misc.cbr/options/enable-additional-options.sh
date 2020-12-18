8a455acc96c18bbeee5e20078eb4794fb7f4356c
hexdump <hexdump0815@googlemail.com>
2020-12-18 20:42:25 +0100

    cleanup: set version, run extra option scripts

--- misc.cbr/options/enable-additional-options.sh
+++ misc.cbr/options/enable-additional-options.sh
@@ -10,8 +10,6 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
-./scripts/config -d CONFIG_DRM_TEGRA
-./scripts/config -d CONFIG_DRM_NOUVEAU
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0x0
 ./scripts/config -d CONFIG_DRM_TEGRA
 ./scripts/config -d CONFIG_DRM_NOUVEAU
@@ -21,6 +19,9 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_XFS_FS
 ./scripts/config -d CONFIG_REISERFS_FS
 ./scripts/config -d CONFIG_OCFS2_FS
+
+./scripts/config -d CONFIG_LOCALVERSION_AUTO
+./scripts/config --set-str CONFIG_LOCALVERSION "-stb-cbr"
 
 for i in `cat /compile/doc/stable/misc.cbr/options/additional-options-*-yes.txt`; do
   echo $i
--- misc.cbt/options/enable-additional-options.sh
+++ misc.cbt/options/enable-additional-options.sh
@@ -11,14 +11,15 @@ cd /compile/source/linux-stable
 ./scripts/config -d CONFIG_BLK_DEV_RAM_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_RAM_SIZE
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0x0
-./scripts/config -d CONFIG_DRM_TEGRA
-./scripts/config -d CONFIG_DRM_NOUVEAU
 ./scripts/config -d CONFIG_DRM_RADEON
 ./scripts/config -d CONFIG_DRM_AMDGPU
 ./scripts/config -d CONFIG_DRM_ETNAVIV
 ./scripts/config -d CONFIG_XFS_FS
 ./scripts/config -d CONFIG_REISERFS_FS
 ./scripts/config -d CONFIG_OCFS2_FS
+
+./scripts/config -d CONFIG_LOCALVERSION_AUTO
+./scripts/config --set-str CONFIG_LOCALVERSION "-stb-cbt"
 
 for i in `cat /compile/doc/stable/misc.cbt/options/additional-options-*-yes.txt`; do
   echo $i
